de4be820d4a240f6456a066a43feb2a3d84331d1
Zachary Patterson <localhost@zspatter.dev>
2019-04-02 16:52:01 -0400

    Adds Stack implemented in Java

---
+++ stack-vs-queue/Stack.java
@@ -0,0 +1,70 @@
+import java.util.Arrays;
+
+public class Stack
+{
+    private int capacity;
+    private int top;
+    private int[] stack;
+
+    public Stack()
+    {
+        this(1000);
+    }
+
+    public Stack(int capacity)
+    {
+        this.capacity = capacity;
+        this.top = -1;
+        this.stack = new int[capacity];
+    }
+
+    public boolean isEmpty()
+    {
+        return top < 0;
+    }
+
+    public boolean isFull()
+    {
+        return this.top >= this.capacity - 1;
+    }
+
+    public boolean push(int element)
+    {
+        if (isFull())
+        {
+            System.out.println("Stack Overflow");
+            return false;
+        }
+
+        stack[++top] = element;
+        return true;
+    }
+
+    public int pop()
+    {
+        if (isEmpty())
+        {
+            System.out.println("Stack Underflow");
+            return Integer.MIN_VALUE;
+        }
+
+        return stack[top--];
+    }
+
+    public int peak()
+    {
+        if (isEmpty())
+        {
+            System.out.println("Stack Underflow");
+            return Integer.MIN_VALUE;
+        }
+
+        return this.stack[top];
+    }
+
+    @Override
+    public String toString()
+    {
+        return Arrays.toString(this.stack);
+    }
+}
